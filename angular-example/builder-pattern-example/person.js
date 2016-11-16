eae77b108e5fccb275908bfb481530d100625be6
Chourasia, Ashok <ashok.chourasia@vce.com>
2016-11-16 21:50:32 +0530

    add a builder script

---
+++ angular-example/builder-pattern-example/person.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function () {
+    function Person() {
+        this.setlastName=function(lastName){
+            this.newLastName=lastName;
+            return this;
+        };
+        this.setFirstName=function(firstName){
+            this.newFirstName=firstName;
+            return this;
+        };
+        this.setMiddleName=function(middleName){
+            this.newMiddleName=middleName;
+            return this;
+        };
+    }
+    var p1=new Person().setFirstName("ashok").setlastName("chourasia").setMiddleName("kumar");
+    console.log("p1=  "+JSON.stringify(p1));
+     
+    var p2=new Person();
+     p2.setFirstName("Ram");
+     p2.setlastName("Singh");
+     p2.setMiddleName("kumar");
+      console.log("p2=  "+JSON.stringify(p2));
+})();
+
