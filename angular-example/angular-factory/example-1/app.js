09ebc823d5f1a6e1c2930d0435ac46873c2ac725
Chourasia, Ashok <ashok.chourasia@vce.com>
2016-11-16 16:10:57 +0530

    added new files

--- angular-example/angular-factory/example-1/app.js
+++ angular-example/angular-factory/example-1/app.js
@@ -3,21 +3,21 @@
     myApp.factory('MathService', function() {
         return {
             add: function(a, b) {
-                return a + b
+                return a + b;
             },
 
             subtract: function(a, b) {
-                return a - b
+                return a - b;
             },
 
             multiply: function(a, b) {
-                return a * b
+                return a * b;
             },
 
             divide: function(a, b) {
-                return a / b
+                return a / b;
             }
-        }
+        };
     });
 
     myApp.factory('CalculatorService', function(MathService) {
@@ -28,16 +28,16 @@
             cube: function(a) {
                 return MathService.multiply(a, MathService.multiply(a, a));
             }
-        }
+        };
     });
     myApp.controller('CalculatorController', function($scope, CalculatorService) {
 
         $scope.doSquare = function() {
             $scope.answer = CalculatorService.square($scope.number);
-        }
+        };
 
         $scope.doCube = function() {
             $scope.answer = CalculatorService.cube($scope.number);
-        }
+        };
     });
 })();
