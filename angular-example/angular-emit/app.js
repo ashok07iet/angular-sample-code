30b4c80b5d0071b8b627975839eb9175f83d814d
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-01-02 23:23:37 +0530

    added broad cast

---
+++ angular-example/angular-emit/app.js
@@ -0,0 +1,16 @@
+(function () {
+    var app = angular.module('app', []);
+
+    app.controller("firstCtrl", function ($scope) {
+        $scope.$on('eventName', function (event, args) {
+            $scope.message = args.message;
+            console.log($scope.message);
+        });
+    });
+
+    app.controller("secondCtrl", function ($scope) {
+        $scope.handleClick = function (msg) {
+            $scope.$emit('eventName', {message: msg});
+        };
+    });
+})();
