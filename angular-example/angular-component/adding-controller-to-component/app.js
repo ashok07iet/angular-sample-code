4f3655889c8397c70a273aa3d7a0a3381708d004
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-01-02 07:47:03 +0530

    added component

---
+++ angular-example/angular-component/adding-controller-to-component/app.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function () {
+    angular.module("myApp", [])
+            .component("helloWorld", {
+                template: "Hello {{$ctrl.name}}, I'm {{$ctrl.myName}}!",
+                bindings: {name: '@'},
+                controller: function () {
+                    this.myName = 'Alain';
+                }
+            });
+})();
