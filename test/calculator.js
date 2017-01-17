aec4bce71161f7ebbe1eeca00268391659a5f695
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-01-17 07:45:44 +0530

    added test file

---
+++ test/calculator.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var calculator = {
+    add: function (a, b) {
+        return a + b;
+    },
+    multiply: function (a, b) {
+        return a * b;
+    },
+    devide: function (a, b) {
+        return a / b;
+    },
+    subtract: function (a, b) {
+        return a - b;
+    }
+};
+
---
+++ test/calculator.spec.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+describe('Calculator ', function() {
+
+// necessary
+it('should add two numbers correctly', function() {});
+it('should subtract two numbers correctly', function() {});
+
+// helpful but not needed
+it('should add negative numbers', function() { });
+it('should reject non numbers', function() { });
+});
